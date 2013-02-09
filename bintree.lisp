06d44ab120031d083a532e542e2e0d8585584278
Mark Brown <hong.kong@phooey.com>
2013-02-09 21:16:56 +0900

    Correctly finds immediate predecessor element. Need to think about deleting from the left subtree.

--- bintree.lisp
+++ bintree.lisp
@@ -1,4 +1,8 @@
 ;;;; Binary search tree.
+;;;; Implemented as a purely functional (persistent) structure.
+;;;; Insertion and deletion operations return a new tree and leave
+;;;; the original unchanged.
+
 
 (defpackage :bintree
   (:use :common-lisp)
@@ -11,7 +15,7 @@
 
 ;;; A node is defined as a tuple (left element right).
 
-(declaim (inline node-left node-right node-element new-node))
+(declaim (inline node-left node-right node-element new-node test-node))
 
 (defun node-left    (node) (first node))
 (defun node-right   (node) (third node))
@@ -22,6 +26,11 @@
 
 
 (defun test-node (element node cmp-element)
+  "Compare element with node.
+   Return 0 if the element and node contents are equal
+   a negative value if the element is less than the 
+   or a positive value if the element is greater than
+   the node value."
   (funcall cmp-element element (node-element node)))
 
 
@@ -29,6 +38,7 @@
                     tree 
                     &optional (add-element #'(lambda (v &rest cntr) v))
                               (cmp-element #'-))
+  "Insert element into tree, returning a new tree."
   (if (null tree)
       (new-node nil element nil)
       (let ((cmp (test-node element tree cmp-element)))
@@ -47,20 +57,23 @@
 
 
 (defun tree-search (element tree &optional (cmp-element #'-))
+  "Search tree for element.
+   Returns nil if the element is not found, otherwise returns
+   the tree node's value."
   (if (null tree)
       nil
       (let* ((node-value (node-element tree))
              (cmp        (funcall cmp-element element node-value)))
-	(cond ((zerop cmp)  node-value)
-	      ((minusp cmp) (tree-search element (node-left tree)  cmp-element))
-	      (t            (tree-search element (node-right tree) cmp-element))))))
+        (cond ((zerop cmp)  node-value)
+              ((minusp cmp) (tree-search element (node-left tree)  cmp-element))
+              (t            (tree-search element (node-right tree) cmp-element))))))
 
 
 (defun node-children (node) 
   "Returns the number of children of a node."
   (reduce #'+ 
-	  (mapcar #'(lambda (x) (if (null x) 0 1))
-		  (list (node-left node) (node-right node)))))
+          (mapcar #'(lambda (x) (if (null x) 0 1))
+                  (list (node-left node) (node-right node)))))
 
 
 (defun node-equal-p (element node cmp-element)
@@ -75,23 +88,44 @@
       (values  1 (node-equal-p element (node-right this-node) cmp-element))))
     
 
+(defun x-get-predecessor (node)
+  (labels ((right-descend (node)
+             (let ((right-child (node-right node)))
+               (if (null right-child)
+                    node
+                    (right-descend right-child)))))
 
+    (right-descend (node-left node))))
+      
 
-(defun replace-with-predecessor (tree)
-  (multiple-value-bind (predecessor-element subtree) (x-get-predecessor (node-left tree))
-    (new-node subtree predecessor-element (node-right tree))))
-)
+(defun replace-with-predecessor (tree cmp-element)
+  "Return a new node consisting of the immediate predecessor of 
+   the element in tree, the left subtree with the predecessor
+   removed, and the right subtree as it is."
+  (let* ((predecessor-node (x-get-predecessor tree))
+         (predecessor-element (node-element predecessor-node)))
+    (new-node (tree-delete predecessor-element (node-left tree) cmp-element)
+              predecessor-element
+              (node-right tree))))
 
 
 (defun delete-node (tree cmp-element)
   "Remove node from tree and return its replacement node."
   (let ((n-children (node-children tree)))
-    (cond ((zerop n-children) nil)
+    ;; We treat three cases: the node to delete has no children,
+    ;; one child or two children.
+    (cond ;;The no-child case is the simplest - simply delete the node.
+          ((zerop n-children) nil)
+          ;; If one child, replace the node with the child.
           ((= 1 n-children) 
            (if (null (node-left tree)) 
                (node-right tree) 
                (node-left tree)))
-          (t (replace-with-predecessor tree)))))
+          ;; With two children, replace the node with either its
+          ;; immediate predecessor (left subtree) and leave the
+          ;; right subtree unchanged, or with its immediate successor
+          ;; (right subtree) and leave the left subtree unchanged.
+          (t (replace-with-predecessor tree cmp-element)))))
 
 
 (defun tree-delete (element tree &optional (cmp-element #'-))
