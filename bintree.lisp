a3eed103a5ca58a3b496dd92d7ae19e76f3e677b
Mark Brown <hong.kong@phooey.com>
2013-02-10 09:27:20 +0900

    replace-with-predecessor implemented. Now needs integrating into delete-node

--- bintree.lisp
+++ bintree.lisp
@@ -13,7 +13,10 @@
 (in-package :bintree)
 
 
+;;; ---------------------------------------------------------------------
+;;; Node utility functions.
 ;;; A node is defined as a tuple (left element right).
+
 
 (declaim (inline node-left node-right node-element new-node test-node))
 
@@ -21,17 +24,21 @@
 (defun node-right   (node) (third node))
 (defun node-element (node) (second node))
 
+
 (defun new-node (left element right)
   (list left element right))
 
 
 (defun test-node (element node)
   "Compare element with node.
-   Return 0 if the element and node contents are equal
+   Return 0 if the element and node contents are equal,
    a negative value if the element is less than the 
    or a positive value if the element is greater than
    the node value."
   (- element (node-element node)))
+
+
+;;; ---------------------------------------------------------------------
 
 
 (defun tree-insert (element tree)
@@ -53,6 +60,9 @@
                          (tree-insert element (node-right tree))))))))
 
 
+;;; ---------------------------------------------------------------------
+
+
 (defun tree-search (element tree)
   "Search tree for element.
    Returns nil if the element is not found, otherwise returns
@@ -64,6 +74,29 @@
         (cond ((zerop cmp)  node-value)
               ((minusp cmp) (tree-search element (node-left tree)))
               (t            (tree-search element (node-right tree)))))))
+
+
+;;; ---------------------------------------------------------------------
+;;; Node deletion.
+;;; We have to consider three cases:
+;;; (a) The node to delete has no children. In this case, we simply 
+;;; remove the node.
+;;; (b) The node to delete has a single child. In this case we replace
+;;; the node with its child.
+;;; (c) The node to delete has two children. In this case we can either
+;;;     (i) replace the node with its immediate predecessor, in which
+;;;         case we delete the predecesor from the left subtree and
+;;;         leave the right subtree unchanged, or
+;;;     (ii) replace the node with its immediate successor, in which
+;;;         case we leave the left subtree unchanged and delete the 
+;;;         predecessor from the right subtree.
+;;;     The immediate successor/predecessor will only have one child
+;;;     at most, so deleting it can be handled as case (a) or (b) 
+;;;     above.
+;;; Because we are dealing with a purely functional tree, we have to
+;;; create a new copy of all the ancestors of the node to delete as 
+;;; well as the node itself. We need to find the *parent* of the node
+;;; to be deleted.
 
 
 (defun node-children (node) 
@@ -79,13 +112,39 @@
 
 
 (defun parent-of-p (element this-node)
-  "Returns whether this-node is a parent of element node."
+  "Returns whether this-node is a parent the node containing
+   element node."
   (if (minusp (test-node element this-node))
       (values -1 (node-equal-p element (node-left this-node)))
       (values  1 (node-equal-p element (node-right this-node)))))
     
 
-(defun x-get-predecessor (node)
+(defun get-successor (node)
+  "Return leftmode node in right subtree, which corresponds
+   to this node's immediate successor."
+  (labels ((left-descend (n)
+            (let ((left-child (node-left n)))
+              (if (null left-child)
+                  n
+                  (left-descend left-child)))))
+
+    (left-descend (node-right node))))
+
+
+(defun replace-with-successor (tree)
+  "Return a new node consisting of the immediate successor of
+   the element in tree, the right subtree with the successor
+   removed, and the left subtree as it is."
+  (let* ((successor-node (get-successor tree))
+         (successor-element (node-element successor-node)))
+    (new-node (node-left tree)
+              successor-element
+              (tree-delete successor-element (node-right tree)))))
+
+
+(defun get-predecessor (node)
+  "Return rightmost node in left subtree, which corresponds
+   to this node's immediate predecessor."
   (labels ((right-descend (node)
              (let ((right-child (node-right node)))
                (if (null right-child)
@@ -99,7 +158,7 @@
   "Return a new node consisting of the immediate predecessor of 
    the element in tree, the left subtree with the predecessor
    removed, and the right subtree as it is."
-  (let* ((predecessor-node (x-get-predecessor tree))
+  (let* ((predecessor-node (get-predecessor tree))
          (predecessor-element (node-element predecessor-node)))
     (new-node (tree-delete predecessor-element (node-left tree))
               predecessor-element
